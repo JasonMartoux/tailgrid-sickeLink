96a9c423062f6d5ea0026a4846fbf7e5b3333156
Jason Martoux <apps+git@martoux.com>
2024-10-26 17:56:15 +0200

    add ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compress()],
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
