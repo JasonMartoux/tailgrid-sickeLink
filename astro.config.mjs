981a52ef0390f117c43c6773109be20ce80c9d8b
Jason Martoux <apps+git@martoux.com>
2024-10-27 12:35:59 +0100

    add githubaction

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compress()],
 });
